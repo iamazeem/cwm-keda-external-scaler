8f056631219868b8e50d545b3a8a445b1bfc00b9
Azeem Sajid <azeem.sajid@gmail.com>
2021-05-12 15:30:46 +0500

    Updated script to push docker image with both GITHUB_SHA and latest tags.

--- bin/push_docker_image.sh
+++ bin/push_docker_image.sh
@@ -11,17 +11,28 @@ eval "$(minikube -p minikube docker-env)"
 
 PKGS_URL="docker.pkg.github.com"
 SOURCE_IMAGE="cwm-keda-external-scaler"
-TAG="latest"
-TARGET_IMAGE="$PKGS_URL/$REPO/$SOURCE_IMAGE:$TAG"
 
-echo "Logging in to docker registry"
-echo "$PASSWORD" | docker login https://docker.pkg.github.com -u "$USERNAME" --password-stdin
+log_in () {
+    echo "Logging in to docker registry"
+    echo "$PASSWORD" | docker login https://docker.pkg.github.com -u "$USERNAME" --password-stdin
+}
 
-echo "Retagging docker image"
-docker tag "$SOURCE_IMAGE" "$TARGET_IMAGE"
-docker images
+push_image () {
+    local TAG="$1"
+    local TARGET_IMAGE="$PKGS_URL/$REPO/$SOURCE_IMAGE:$TAG"
 
-echo "Pushing image [$TARGET_IMAGE]"
-docker push "$TARGET_IMAGE"
+    echo "Retagging image [$SOURCE_IMAGE -> $TARGET_IMAGE]"
+    docker tag "$SOURCE_IMAGE" "$TARGET_IMAGE"
+    docker images
 
-echo "Docker image [$TARGET_IMAGE] pushed successfully!"
+    echo "Pushing image [$TARGET_IMAGE]"
+    docker push "$TARGET_IMAGE"
+
+    echo "Docker image [$TARGET_IMAGE] pushed successfully!"
+}
+
+log_in
+push_image "$GITHUB_SHA"
+push_image "latest"
+
+echo "--- [DONE] ---"
